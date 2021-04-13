56381ad5d0a2995d4afd7f29f8fc4db32fd1995c
Beth Williams <lizb3th@live.co.uk>
2021-04-13 11:14:28 +1000

    public

--- include/event_v2.hpp
+++ include/event_v2.hpp
@@ -8,6 +8,7 @@
 #include<atomic>
 #include<memory>
 #include<algorithm>
+#include<condition_variable>
 
 #include <cassert>
 
@@ -75,7 +76,11 @@ namespace rpt::event_detail {
     struct atomic_shared_array {
         class detail {
         public:
-            std::shared_ptr<T[]> load(std::memory_order order = std::memory_order_seq_cst) noexcept { return std::atomic_load_explicit(&data, order); }
+            detail() noexcept = default;
+            detail(std::shared_ptr<T[]> desired) noexcept
+                : data{std::move(desired)} {}
+
+            std::shared_ptr<T[]> load(std::memory_order order = std::memory_order_seq_cst) const noexcept { return std::atomic_load_explicit(&data, order); }
 
             void store(std::shared_ptr<T[]> desired, std::memory_order order = std::memory_order_seq_cst) noexcept
             { return std::atomic_store_explicit(&data, desired, order); }
@@ -111,6 +116,64 @@ namespace rpt::event_detail {
     using array_entry_type = listener_base<Params...>*;
 
     template<typename T>
+    class waitable_atomic {
+    public:
+        constexpr waitable_atomic(T desired) noexcept;
+        void store(T desired, std::memory_order order = std::memory_order_seq_cst) noexcept;
+        T load(std::memory_order order = std::memory_order_seq_cst) const noexcept;
+        T operator++(int) noexcept;
+        void notify_all();// noexcept;
+        void wait(T old, std::memory_order order = std::memory_order_seq_cst) const;// noexcept;
+
+    private:
+        std::atomic<T> atom;
+        mutable std::condition_variable cv;
+        mutable std::mutex mtx;
+    };
+
+    template<typename T>
+    constexpr waitable_atomic<T>::waitable_atomic(T desired) noexcept
+        : atom{ desired }
+    {}
+
+    template<typename T>
+    void waitable_atomic<T>::store(T desired, std::memory_order order) noexcept {
+        atom.store(std:move(desired), order);
+    }
+
+    template<typename T>
+    T waitable_atomic<T>::load(std::memory_order order) const noexcept {
+        return atom.load(order);
+    }
+
+    template<typename T>
+    T waitable_atomic<T>::operator++(int) noexcept {
+        static_assert(std::is_integral_v<T>);
+        return atom++;
+    }
+
+    template<typename T>
+    void waitable_atomic<T>::notify_all() {
+        std::unique_lock{ mtx }; //dont hold lock;
+        cv.notify_all();
+    }
+
+    template<typename T>
+    void waitable_atomic<T>::wait(T old, std::memory_order order) const {
+        if (atom.load(order) != old) {
+            auto lock = std::unique_lock{ mtx };
+            cv.wait(lock, [&] { return atom.load(order) != old; });
+        }
+    }
+
+    //using atomic_uintptr_t = waitable_atomic<std::uintptr_t>;
+    /*
+        The counter type is tricky since it may be valid for the bits of a 
+        pointer to not correspondt to nice integer values.
+    */
+    using counter_t = std::uint64_t;
+
+    template<typename T>
     struct array_generator
     {
         static_assert(std::is_pointer_v<T>);
@@ -125,6 +188,7 @@ namespace rpt::event_detail {
         allocator_type get_allocator() const;
 
         array_type make_array(std::remove_pointer_t<T>&) const;
+        array_type make_array() const;
 
         array_type copy_push_back(const array_type&, std::remove_pointer_t<T>&) const;
 
@@ -141,12 +205,19 @@ namespace rpt::event_detail {
 
     template<typename T>
     array_generator<T>::array_generator(allocator_type alloc)
-        : alloc{ alloc } {}
+        : alloc{ alloc } {
+    }
 
     template<typename T>
     typename array_generator<T>::array_type array_generator<T>::make_array(std::remove_pointer_t<T>& r) const {
         auto output = make_array_for_overwrite(1, 1);
         output[2] = &r;
+        return output;
+    }
+
+    template<typename T>
+    typename array_generator<T>::array_type array_generator<T>::make_array() const {
+        auto output = make_array_for_overwrite(0, 0);
         return output;
     }
 
@@ -166,27 +237,39 @@ namespace rpt::event_detail {
     template<typename T>
     typename array_generator<T>::array_type array_generator<T>::copy_remove(const typename array_generator<T>::array_type& arr, std::remove_pointer_t<T>& t) const {
         auto size = get_size(arr) - 1;
-        if (size != 0)
-        {
+        //if (size != 0)
+        //{
             auto output = make_array_for_overwrite(size, get_generation(arr) + 1);
             std::remove_copy(get_data(arr), std::next(get_data(arr), size + 1), get_data(output), &t);
             return output;
-        }
-        return nullptr;
+       // }
+       // return nullptr;
     }
 
     template<typename T>
     std::size_t array_generator<T>::get_size(const array_type& arr) {
+        assert(arr != nullptr);
+        //if (arr == nullptr)
+        //    return 0;
+
         return reinterpret_cast<std::size_t>(arr[0]);
     }
 
     template<typename T>
     std::uintptr_t array_generator<T>::get_generation(const array_type& arr) {
+        assert(arr != nullptr);
+        //if (arr == nullptr)
+        //    return 0;
+
         return reinterpret_cast<std::uintptr_t>(arr[1]);
     }
 
     template<typename T>
     T* array_generator<T>::get_data(const array_type& arr) {
+        assert(arr != nullptr);
+        //if (arr == nullptr)
+        //    return 0;
+
         return std::next(arr.get(), 2);
     }
 
@@ -255,6 +338,12 @@ namespace rpt::event_detail {
         array_viewer(std::shared_ptr<entry_type[]>&&, std::size_t);
         array_viewer(const std::shared_ptr<entry_type[]>&, std::size_t);
 
+        array_viewer(array_viewer&&) = default;
+        array_viewer(const array_viewer&) = default;
+
+        array_viewer& operator=(array_viewer&&) = default;
+        array_viewer& operator=(const array_viewer&) = default;
+
         iterator begin();
 
         iterator end();
@@ -293,6 +382,9 @@ namespace rpt::event_detail {
     std::size_t array_viewer<T>::size() {
         return sz;
     }
+    
+    using atomic_counter_t = waitable_atomic<uint64_t>;
+    using atomic_uintptr_t = waitable_atomic<uint64_t>;
 
     /*
         Technically needed because the lifetime of the event class ends
@@ -312,10 +404,8 @@ namespace rpt::event_detail {
         */
         void repudiate(listener_base<Params...>&);
 
-        /*
-            get a copy of hte list that cannot be detructed while the shared pointer (array_type) is held.
-        */
-        array_type lock();
+        template<typename... Args>
+        void operator()(Args&&...);
 
         /*
             Add a listener to the list of subscribers
@@ -328,24 +418,76 @@ namespace rpt::event_detail {
         */
         void clear();
 
+        /*
+            returns tru if there are currently no listeners subscribed.
+        */
+        bool empty() const;
+
+        array_viewer<listener_base<Params...>> view_lock();
+
     private:
+        using generator_type = array_generator<listener_base<Params...>*>;
+        using listener_type = listener_base<Params...>;
+        
+        /*
+            for 20 ArrModFn should be a concept
+        */
+        template<typename ArrModFn>
+        void replace_data_using(ArrModFn&& fn);
+
+        /*
+            get a copy of hte list that cannot be detructed while the shared pointer (array_type) is held.
+        */
+        array_type lock();
+
+        /*
+            block untill the generation atomic changes value. Then return the new value.
+        */
+        void wait_on_generatrion_equal(uint64_t value);
+
+        /*
+            block until the number of viewers is equal to the requested value
+
+            only call when viewers can only decreas
+        */
+        void wait_on_viewer_count(const array_type& arr, long count);
+
         using atomic_array_type = atomic_shared_array_t<listener_base<Params...>*>;
 
-        array_generator<listener_base<Params...>*> generator;
-
-        std::atomic_uintptr_t free_generation{ 0 };
-        atomic_array_type data{};
+        generator_type generator;
+        
+        atomic_uintptr_t call_complete{ 0 };
+        atomic_uintptr_t free_generation{ 0 };
+        atomic_array_type data{ generator.make_array() };
     };
 
     template<typename... Params>
-    void event_base<Params...>::repudiate(listener_base<Params...>& l) {
+    array_viewer<listener_base<Params...>> event_base<Params...>::view_lock()
+    {
         auto holder = lock();
-        assert(holder);
-        auto next = generator.copy_remove(holder, l);
-        while (!data.compare_exchange_strong(holder, next))
-        {
-            next = generator.copy_remove(holder, l);
-        }
+        auto size = generator.get_size(holder);
+        auto ptr = generator.get_data(holder);
+        return array_viewer<listener_base<Params...>> {
+            std::shared_ptr<listener_base<Params...>*[]>{std::move(holder), ptr},
+            size
+        };
+    }
+
+    template<typename... Params>
+    template<typename... Args>
+    void event_base<Params...>::operator()(Args&&... args) {
+        auto view = event_detail::array_viewer{ view_lock() };
+        std::for_each(view.begin(), view.end(), [args...](auto& l) {
+            l(args...);
+       });
+       call_complete++;
+       call_complete.notify_all();
+    }
+
+    template<typename... Params>
+    bool event_base<Params...>::empty() const {
+        return (generator.get_size(data.load()) == 0);
+        //return data.load() == nullptr;
     }
 
     template<typename... Params>
@@ -354,19 +496,76 @@ namespace rpt::event_detail {
     }
 
     template<typename... Params>
-    void event_base<Params...>::subscribe(listener_base<Params...>& l) {
+    void event_base<Params...>::wait_on_generatrion_equal(uint64_t value) {
+        auto current_generation = free_generation.load();
+        while (current_generation != value) {
+            free_generation.wait(current_generation);
+            current_generation = free_generation.load();
+        }
+    }
+
+    template<typename... Params>
+    void event_base<Params...>::wait_on_viewer_count(const array_type& arr, long target_count) {
+        auto current_complete = call_complete.load();
+        auto current_count = arr.use_count();
+        while (current_count > target_count) {
+            call_complete.wait(current_complete);
+            current_complete = call_complete.load();
+            current_count = arr.use_count();
+        }
+    }
+
+    template<typename... Params>
+    template<typename ArrModFn>
+    void event_base<Params...>::replace_data_using(ArrModFn&& fn) {
         auto holder = lock();
-        auto next = holder ? generator.copy_push_back(holder, l) : generator.make_array(l);
+        auto next = fn(holder);
         while (!data.compare_exchange_strong(holder, next))
         {
-            next = holder ? generator.copy_push_back(holder, l) : generator.make_array(l);
+            next = fn(holder);
         }
+
+        wait_on_viewer_count(holder, 1);
+
+        auto generatrion = generator.get_generation(holder);
+        holder = nullptr;
+
+        wait_on_generatrion_equal(generatrion);
+
+        free_generation++;
+    }
+
+    template<typename... Params>
+    void event_base<Params...>::subscribe(listener_base<Params...>& l) {
+        replace_data_using([&](const auto& arr) {
+            return generator.copy_push_back(arr, l);
+        });
+    }
+
+    template<typename... Params>
+    void event_base<Params...>::repudiate(listener_base<Params...>& l) {
+        replace_data_using([&](const auto& arr) {
+            return generator.copy_remove(arr, l);
+        });
     }
 
     template<typename... Params>
     void event_base<Params...>::clear() {
         auto holder = lock();
-        
+        auto size = generator.get_size(holder);
+
+        auto removed = 0;
+        std::for_each_n(generator.get_data(holder), generator.get_size(holder),[&removed](auto l){
+            if (l->detatch())
+                removed++;
+        });
+
+        auto generatrion = generator.get_generation(holder);
+        holder = nullptr;
+
+        wait_on_generatrion_equal(generatrion);
+
+        //data.store(nullptr);
     }
 }
 
@@ -409,7 +608,6 @@ namespace rpt {
                 return static_cast<listener<Callback, Params...>&>(l).repudiate_fn.exchange(nullptr) != nullptr;
             }
     {
-
     }
 
     template<typename Callback, typename... Params>
@@ -425,24 +623,63 @@ namespace rpt {
     template<typename... Params>
     class event : event_detail::event_base<Params...>{
     public:
-        event();
+        event() = default;
         event(const event&) = delete;
         event(event&&) = delete;
         event& operator=(const event&) = delete;
         event& operator=(event&&) = delete;
+
+        ~event();
 
         template<typename... Args>
         void operator()(Args&&...);
 
         template<typename Callback>
         listener<Callback> subscribe(Callback&&);
+    private:
+        using base_type = event_detail::event_base<Params...>;
+
+        std::atomic_uintptr_t invokers{ 0 };
+    };
+
+    template<>
+    class event<void> : event_detail::event_base<> {
+    public:
+        event() = default;
+        event(const event&) = delete;
+        event(event&&) = delete;
+        event& operator=(const event&) = delete;
+        event& operator=(event&&) = delete;
+
+        ~event();
+
+        void operator()();
+
+        template<typename Callback>
+        listener<Callback> subscribe(Callback&&);
+    private:
+        using base_type = event_detail::event_base<>;
+
+        std::atomic_uintptr_t invokers{ 0 };
     };
 
     template<typename... Params>
-    template<typename... Args>
-    void event<Params...>::operator()(Args&&...)
-    {
+    event<Params...>::~event() {
+        base_type::clear();
+    }
 
+    event<void>::~event() {
+        base_type::clear();
+    }
+
+    template<typename... Params>
+    template<typename... Args>
+    void event<Params...>::operator()(Args&&... args) {
+        base_type::operator()(std::move(args)...);
+    }
+
+    void event<void>::operator()() {
+        base_type::operator()();
     }
 
     template<typename... Params>
